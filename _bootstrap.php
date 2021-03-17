04b03b288f815c973470ce45ecfb590f57b4c24b
Pramod Ranasinghe <38061830+pramodS92@users.noreply.github.com>
2021-03-17 14:45:10 +0530

    Update _bootstrap.php
    COMMIT CHANGED

--- _bootstrap.php
+++ _bootstrap.php
@@ -56,7 +56,7 @@ if (intval($apiVersion) < 39) {
 }
 //changed
 // build api endpoint url
-$gatewayUrl = "https://cbcmpgs.gateway.mastercard.com/api/rest/version/49/merchant/${merchantId}";
+$gatewayUrl = "https://${prefix}gateway.mastercard.com/api/rest/version/${apiVersion}/merchant/${merchantId}";
 
 // parse query string
 $query = array();
